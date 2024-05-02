2cfd9658e5255cf6308f580b27c45e873a6bb8f2
guiemhomar <ghomar@nigul.cide.es>
2024-05-02 16:55:18 +0200

    BBDD2

--- clase-expres/index.js
+++ clase-expres/index.js
@@ -4,9 +4,16 @@ const db = require('better-sqlite3')('personas.sqlite');
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs");
+
 // crearemos la configuracion de la bbdd
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
+app.get("/", (req, res) =>{
+    res.render("index", msgs={msgs: ["Hola", "desde", "la", "ruta"]});
+})
 
 app.get('/patata', (req, res) => {
     res.send('Hello World!')
@@ -19,19 +26,32 @@ app.get('/personas', (req, res) => {
     res.send(rows)
 })
 
+//devolveremos el render de la vist con un form
 app.get('/persona', (req, res) => {
     //aqui hare el select
-    personaId = req.query.id;
-    const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaId);
-    res.send(row)
+    // personaId = req.query.id;
+    // const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaId);
+    // res.send(row)
+
+    res.render("persona")
 })
 
-app.post("/postPersona", (req, res) => {
-    personaId = req.body.id;
-    console.log(personaId);
-    const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaId);
-    console.log(row);
-    res.send(row)
+//caputraremos el submit del formulario
+app.post("/persona", (req, res) => {
+    // personaId = req.body.id;
+    // console.log(personaId);
+    // const row = db.prepare('SELECT * from personas WHERE id = ?').get(personaId);
+    // console.log(row);
+    // res.send(row)
+
+    console.log(req.body);
+    if (req.body.nombre && req.body.apellidos){
+        //insert
+        const insert = db.prepare("INSERT INTO personas (nombre, apellidos) VALUES(?, ?");
+        const info = insert.run(req.body.nombre, req.body.apellidos);
+        console.log(info);
+    }
+    res.redirect("persona");
 })
 
 app.listen(port, () => {
